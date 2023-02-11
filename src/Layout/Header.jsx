c52d851bcf30616fbfc66d8ecd4e135a67b36387
Bruno Bispo <bruno.gbispo2@gmail.com>
2023-02-11 00:35:09 -0300

    correção fonte e header

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,13 +1,14 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/anchor-has-content */
 import React from "react";
 import './Header.css'
-
+import image from '../Images/testevsb.png'
 const Header = () =>{
     return(
         <header>
             <div className="logo-vsb">
-                <h1>VSBrepresentações</h1>
+                <img src={image} alt="" />
             </div>
             <nav className="nav-header">
                 <a href="#">Home</a>
