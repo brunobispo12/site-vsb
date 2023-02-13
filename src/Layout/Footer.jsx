930ba387519f2e293fc88359854ab30c1fef5f67
Bruno Bispo <bruno.gbispo2@gmail.com>
2023-02-13 03:20:18 -0300

    responsividade footer

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -1,9 +1,25 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/anchor-has-content */
 import React from "react";
 import './Footer.css'
-
+import WhatsAppIco from '../Images/bxl-whatsapp-square.png';
+import InstagramIco from '../Images/bxl-gmail.png';
+import GmailIco from '../Images/bxl-instagram.png';
+;
 const Footer = () =>{
     return(
-        <footer></footer>
+        <footer className="footer">
+            <div className="Contatcs">
+                <h4>Contatos</h4><br/>
+                <p><h5>Email:</h5> vsbrepre@gmail.com</p>
+                <p><h5>Celular:</h5> (54) 99178-5666</p>
+            </div>
+            <div className="Icon-links">
+                <a href="#"><img src={WhatsAppIco} alt="" /></a>
+                <a href="#"><img src={InstagramIco} alt="" /></a>
+                <a href="#"><img src={GmailIco} alt="" /></a>
+            </div>
+        </footer>
     )
 }
 
