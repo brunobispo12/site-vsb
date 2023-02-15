dc8b91fabf59764f2b85b1969e615f23bec17314
Bruno Bispo <bruno.gbispo2@gmail.com>
2023-02-15 11:04:52 -0300

    atualização text about

--- src/Layout/Home.jsx
+++ src/Layout/Home.jsx
@@ -12,8 +12,10 @@ const Main = () =>{
                     <div className="VSB-About">
                         <div className="text">
                             <h1>VSB Representações</h1>
-                            <p>VSB é uma empresa especializada em fornecer vendas e serviços exepcionais para todos os clientes. Temos experiência em vendas e um conhecimento amplo da indústria metalúrgica e varejo de materiais de construção, trabalhando próximo ao cliente para oferecer a melhor solução possivel e dando suporte no pré e no pós venda.</p>
-                            <p>Se você está procurando uma empresa dedicada a ajudar ao seu négocio crescer a VSB é a empresa certa para você. Entre em contato conosco hoje para obter mais informações.</p>
+                            <p>VSB Representações é uma empresa especializada em representação comercial que visa fornecer serviços de relacionamento e vendas de serviços para os clientes da Serra Gaúcha e litoral norte do Rio Grande do Sul. Temos experiência em vendas no segmento varejo material de construção, indústria metalúrgica, trabalhando próximo ao cliente para oferecer a melhor solução possível e dando suporte no pré-venda e no pós-venda.</p>
+                            <p>Se você está procurando uma empresa dedicada a ajudar ao seu negócio crescer a VSB Representações é a empresa certa para você.
+                            Entre em contato conosco para obter maiores informações sobre a nossas marcas representadas, para sua empresa e ou comercio crescer com o que existe de melhor no mercado, pois ofereceremos os  melhores produtos com um otimo custo benefício.
+                            </p>
                         </div>
                     </div>
                     <div className="About-Images">
